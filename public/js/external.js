73e0d45ec2c90ae1ca726528f63befb128e5751d
j-womack <womack.joshua@gmail.com>
2015-06-16 16:01:26 -0500

    End of day commit

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,17 @@
 console.log('Hello from external JavaScript');
+
+if(true) {
+    console.log("This should happen")
+}
+
+var someBoolean = true;
+if(someBoolean) {
+    console.log("This variable is true")
+}
+
+var someBoolean = false;
+if(someBoolean) {
+    console.log("This variable is " + someBoolean)
+} else {
+    console.log("This variable is " + someBoolean)
+}
